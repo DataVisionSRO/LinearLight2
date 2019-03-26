e4e6e1259c243c98ab73ff15e30a4adafc00367f
Tomas Novak <tomas.novak@datavision.cz>
2019-03-26 10:35:05 +0100

    Added extension to NModbus to support broadcast write message
    Change-Id: I63b88b7c5627cde289a9f145c0429a4cb1d81a22

---
+++ LinearLight2/NModbusExtension/ModbusSerialMasterExtensions.cs
@@ -0,0 +1,12 @@
+﻿using NModbus;
+
+namespace LinearLight2.NModbusExtension
+{
+    public static class ModbusSerialMasterExtensions
+    {
+        public static void BroadcastWriteSingleRegister(this IModbusMaster master, ushort addr, ushort value)
+        {
+            master.Transport.Write(new BroadcastWriteRegisterMessage(addr, value));
+        }
+    }
+}
---
+++ LinearLight2/NModbusExtension/SingleModbusRegister.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using NModbus.Data;
+
+namespace LinearLight2.NModbusExtension
+{
+    internal class SingleModbusRegister : IModbusMessageDataCollection
+    {
+        private readonly ushort value;
+
+        public SingleModbusRegister(ushort value)
+        {
+            this.value = value;
+        }
+
+        public byte[] NetworkBytes {
+            get
+            {
+                var bytes = new MemoryStream(ByteCount);
+
+                var b = BitConverter.GetBytes((ushort)IPAddress.HostToNetworkOrder((short)value));
+                bytes.Write(b, 0, b.Length);
+               
+                return bytes.ToArray();
+            }
+        }
+        public byte ByteCount => 2;
+    }
+}
