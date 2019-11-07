0c657b41aa965ea95ffdfc6f5363dba1b89d60e4
Tomas Novak <tomas.novak@datavision.cz>
2019-11-07 17:10:05 +0100

    Removed obsolete items from LinearLight.cs, breaking change to 2.0.0
    Change-Id: Ie007a5b003bd2cece2daf86558732ce4434570b0

--- LinearLight2/LinearLight.cs
+++ LinearLight2/LinearLight.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
@@ -58,63 +57,6 @@ namespace LinearLight2
         {
             set => modbusMaster.BroadcastWriteSingleRegister(Segment.ConfigurationHoldingRegister, (ushort) value);
         }
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<bool> LightOnFlags => Segments.Select(x => x.LightOnFlag);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<bool> HardwareTriggerStatus => Segments.Select(x=>x.HardwareTriggerStatus);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<bool> BodyOverheatFlags => Segments.Select(x=>x.BodyOverheatFlag);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<bool> LedOverheatFlags => Segments.Select(x=>x.LedOverheatFlag);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> ProtocolVersions => Segments.Select(x=>x.ProtocolVersion);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> SoftwareVersions => Segments.Select(x=>x.SoftwareVersion);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> HardwareVersions => Segments.Select(x=>x.HardwareVersion);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<uint> SerialNumber => Segments.Select(x=>Convert.ToUInt32(x.SerialNumber, 16));
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<uint> ProductNumber => Segments.Select(x=>Convert.ToUInt32(x.ProductNumber, 16));
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> BodyTemperatures => Segments.Select(x=>x.BodyTemperature);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> BodyMaxTemperatures => Segments.Select(x=>x.BodyMaxTemperature);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> FanCurrentRpms => Segments.Select(x=>x.FanCurrentRpm);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> LedTemperatures => Segments.Select(x => x.LedTemperature);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> LedMaxTemperatures => Segments.Select(x => x.LedMaxTemperature);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> LuxValues => Segments.Select(x=>x.LuxValue);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<double> Amperes1 => Segments.Select(x => x.Amperes1);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<double> Amperes2 => Segments.Select(x => x.Amperes2);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<double> Volts1 => Segments.Select(x => x.Volts1);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<double> Volts2 => Segments.Select(x => x.Volts2);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<bool> FanEnables => Segments.Select(x => x.FanEnable);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> SetIntensities1 => Segments.Select(x=>x.SetIntensity1);
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<int> SetIntensities2 => Segments.Select(x=>x.SetIntensity2);
-
-        [Obsolete("Use Segments.Select(x=>x.property) instead")]
-        public IEnumerable<ConfigurationStatus> ConfigurationRegisters => Segments.Select(x => x.ConfigurationRegister);
     }
 
     [SuppressMessage("ReSharper", "InconsistentNaming")]
